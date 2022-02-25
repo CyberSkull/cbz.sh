25672c12aa58fd9f79816d43f4bd09c370696149
Trevor <cyberskull@mac.com>
2022-02-25 00:33:48 -0800

    Update cbz.sh
    Most current version I am using.

--- cbz.sh
+++ cbz.sh
@@ -1,13 +1,13 @@
-#! /bin/bash
+#! /bin/zsh
 # Usage: Zips given directories into .cbz (Comic Book ZIP) archives of the smame name (eg "folder name.cbz").
 
-complete="/Applications/iTunes.app/Contents/Resources/complete.aif"
+complete="/System/Applications/Music.app/Contents/Resources/complete.aif"
 remove_flag="m"
 recursive_flag="r"
 test_flag="T"
+symlink_flag="y"
 compression="9"
 open_flag="false"
-
 
 while getopts "kho" flag
 do
@@ -17,44 +17,37 @@ do
 		remove_flag=""
 		;;
 	"h")	#print usage
-		echo "USAGE: cbz [-k][-h] DIRECTORY ..."
+		echo "USAGE: cbz [-k][-h] DIRECTORY…"
 		echo "-h print this help screen"
 		echo "-k keep originals"
+		#echo "-o output directory"
 		exit 0
 		;;
-#		"o")
-#			open_flag="true"
-#			;;
 	esac
 done
 
-#exec > >( lolcat )			# send stdout to lolcat
-#exec 2>&1					# merge stderr into stdout
-
-echo "Running: zip -$remove_flag$recursive_flag$test_flag$compression out.cbz in -x *.DS_Store *[Tt]humbs.db"
-
 for target in "$@"
 do
-	if [[ -e $target ]]
+	if [[ -e $target ]]	#exists
 	then
-		if [[ -d $target ]]
+		if [[ -r $target ]]	#readable
 		then
-			echo "Archiving \"$target\""
-			# -m delete originals, -r recursive, -T test zip, -9 maximum compression, -x exclude list
-			zip -"$remove_flag$recursive_flag$test_flag$compression" "$target.cbz" "$target" -x "*.DS_Store" "*[Tt]humbs.db"
-			
-#				if [[ $open_flag == "true" ]]
-#				then
-#					echo "Opening $target.cbz"
-#					open "$target.cbz"
-#				fi
-			
+			if [[ -d $target ]]	#directory
+			then
+				archive=${target%/}
+				echo "Archiving: $target"
+				echo "zip -$remove_flag$recursive_flag$test_flag$symlink_flag$compression \"$archive.cbz\" \"$target\" -x \"*.DS_Store\" \"*[Tt]humbs.db\""
+				# -m delete originals, -r recursive, -T test zip, -y store symlink, -9 maximum compression, -x exclude list
+				zip -"$remove_flag$recursive_flag$test_flag$symlink_flag$compression" "$archive.cbz" "$target" -x "*.DS_Store" "*[Tt]humbs.db"
+			else
+				echo "Not a directory: $target"
+			fi	#-d
 		else
-			echo "\"$target\" is not a directory or not readable. Skipping."
-		fi #-d
+			echo "Not readable: $target"
+		fi #-r
 	else
-		echo "\"$target\" does not exist."
-	fi
+		echo "Not found: $target"
+	fi	#-e
 done
 
 #When done play the iTunes done sound.
